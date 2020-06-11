a774b29c7291b4533d5ae78ed1333f5cfdd67511
thevirtuoso1973 <chris.tomy@hotmail.com>
2020-06-11 12:56:03 +0100

    separate regex into different file

--- src/case_converter/console.py
+++ src/case_converter/console.py
@@ -1,6 +1,7 @@
 import click
 
 from . import __version__, converters
+from .regex import Case
 
 @click.command()
 @click.version_option(version=__version__)
@@ -9,15 +10,15 @@ from . import __version__, converters
 @click.option("-o", "--output", type=click.Path(allow_dash=True),
               default="-", show_default=True, help="file to output to")
 @click.option("-f", "--from", "from_",
-              type=click.Choice([converters.Case.CAMEL.value,
-                                 converters.Case.SNAKE.value,
-                                 converters.Case.KEBAB.value],
+              type=click.Choice([Case.CAMEL.value,
+                                 Case.SNAKE.value,
+                                 Case.KEBAB.value],
                                 case_sensitive=False),
               prompt=True, help="case of the input file")
 @click.option("-t", "--to",
-              type=click.Choice([converters.Case.CAMEL.value,
-                                 converters.Case.SNAKE.value,
-                                 converters.Case.KEBAB.value],
+              type=click.Choice([Case.CAMEL.value,
+                                 Case.SNAKE.value,
+                                 Case.KEBAB.value],
                                 case_sensitive=False),
               prompt=True, help="desired case")
 @click.option("-e", "--encoding", default="UTF-8", show_default=True,
@@ -31,22 +32,22 @@ def main(filename, output, from_, to, encoding):
     try:
         if filename != "-" and filename == output:
             with click.open_file(filename, "r+", encoding) as inStream:
-                if to == converters.Case.CAMEL.value:
+                if to == Case.CAMEL.value:
                     converters.convertToCamel(inStream, inStream, from_)
-                elif to == converters.Case.SNAKE.value:
+                elif to == Case.SNAKE.value:
                     converters.convertToSnake(inStream, inStream, from_)
-                elif to == converters.Case.KEBAB.value:
+                elif to == Case.KEBAB.value:
                     converters.convertToKebab(inStream, inStream, from_)
                 else:
                     click.echo("Something went wrong.")
         else:
             with click.open_file(filename, "r", encoding) as inStream, \
                 click.open_file(output, "w", encoding) as outStream:
-                if to == converters.Case.CAMEL.value:
+                if to == Case.CAMEL.value:
                     converters.convertToCamel(inStream, outStream, from_)
-                elif to == converters.Case.SNAKE.value:
+                elif to == Case.SNAKE.value:
                     converters.convertToSnake(inStream, outStream, from_)
-                elif to == converters.Case.KEBAB.value:
+                elif to == Case.KEBAB.value:
                     converters.convertToKebab(inStream, outStream, from_)
                 else:
                     click.echo("Something went wrong.")
--- src/case_converter/converters.py
+++ src/case_converter/converters.py
@@ -1,31 +1,4 @@
-import re
-from enum import Enum
-
-class Case(Enum):
-    CAMEL = "camel"
-    SNAKE = "snake"
-    KEBAB = "kebab"
-
-def getCamelRegex():
-    return re.compile("[a-z]+([A-Z][a-z0-9]+)+")
-
-def getSnakeRegex():
-    return re.compile("[a-z]+(_[a-z0-9]+)+")
-
-def getKebabRegex():
-    return re.compile("[a-z]+(-[a-z0-9]+)+")
-
-def getPascalRegex(): # TODO add support for PascalCase
-    return re.compile("([A-Z][a-z0-9_]+)([A-Z][a-z0-9_]+)+")
-
-def getRegex(caseEnum):
-    if caseEnum == Case.CAMEL.value:
-        return getCamelRegex()
-    elif caseEnum == Case.SNAKE.value:
-        return getSnakeRegex()
-    elif caseEnum == Case.KEBAB.value:
-        return getKebabRegex()
-    return None
+from .regex import *
 
 def convertToCamel(inp, out, currCase):
     regex = getRegex(currCase)
